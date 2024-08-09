327bf0b459d8ca9ee005e422590cedb6c4cfa9a7
immanuveljeba <immanuveljebakumar.v@exchange-data.in>
2024-08-09 15:26:36 +0530

    bug fixing

---
+++ hrmsportal/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace hrmsportal;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ hrmsportal/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace hrmsportal;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
