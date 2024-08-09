cf3e578e3c6c6d6a6d06095a65295f84121d3a12
immanuveljeba <immanuveljebakumar.v@exchange-data.in>
2024-08-09 12:53:48 +0530

    initial push

---
+++ hrms_portal/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace hrms_portal;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ hrms_portal/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace hrms_portal;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
