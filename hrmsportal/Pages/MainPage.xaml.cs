bf7606791fbcb9fa28e506647a3a82a54764cb8f
vigneshravi326 <vignesh.r@exchange-data.in>
2024-08-20 17:37:56 +0530

    Added Model, viewModel, Service

--- hrmsportal/Pages/MainPage.xaml.cs
+++ hrmsportal/Pages/MainPage.xaml.cs
@@ -1,77 +1,10 @@
-﻿namespace hrmsportal.Pages;
-using System.ComponentModel;
-using System.Windows.Input;
-using Microsoft.Maui.Controls;
-public partial class MainPage : ContentPage
+﻿namespace hrmsportal.Pages
 {
-	
-	public MainPage()
-	{
-		InitializeComponent();
-        BindingContext = new MainPageViewModel();
-    }
-
-    public class MainPageViewModel : INotifyPropertyChanged
+    public partial class MainPage : ContentPage
     {
-        private bool _isLoginVisible;
-        private bool _isSignupVisible;
-
-        public bool IsLoginVisible
+        public MainPage()
         {
-            get => _isLoginVisible;
-            set
-            {
-                _isLoginVisible = value;
-                OnPropertyChanged(nameof(IsLoginVisible));
-            }
+            InitializeComponent();
         }
-
-        public bool IsSignupVisible
-        {
-            get => _isSignupVisible;
-            set
-            {
-                _isSignupVisible = value;
-                OnPropertyChanged(nameof(IsSignupVisible));
-            }
-        }
-
-        public ICommand ShowLoginCommand { get; }
-        public ICommand ShowSignupCommand { get; }
-
-        public MainPageViewModel()
-        {
-            // Set initial visibility
-            IsLoginVisible = true;
-            IsSignupVisible = false;
-
-            ShowLoginCommand = new Command(ShowLogin);
-            ShowSignupCommand = new Command(ShowSignup);
-        }
-
-        private void ShowLogin()
-        {
-            IsLoginVisible = true;
-            IsSignupVisible = false;
-        }
-
-        private void ShowSignup()
-        {
-            IsLoginVisible = false;
-            IsSignupVisible = true;
-        }
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        protected virtual void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-    }
-
-    void Entry_Focused(System.Object sender, Microsoft.Maui.Controls.FocusEventArgs e)
-    {
     }
 }
-
-
