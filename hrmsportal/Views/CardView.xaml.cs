4cded6e5a5880c107961dc4c79cbff399917587f
immanuveljeba <immanuveljebakumar.v@exchange-data.in>
2024-08-20 12:20:19 +0530

    new files

---
+++ hrmsportal/Views/CardView.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace hrmsportal.Views
+{
+    public partial class CardView : ContentView
+    {
+        public static readonly BindableProperty CardTitleProperty = BindableProperty.Create(nameof(CardTitle), typeof(string), typeof(CardView), string.Empty);
+        public static readonly BindableProperty CardDescriptionProperty = BindableProperty.Create(nameof(CardDescription), typeof(string), typeof(CardView), string.Empty);
+        public static readonly BindableProperty TextColorProperty = BindableProperty.Create(nameof(TextColor), typeof(Color), typeof(CardView), Colors.Black);
+
+        public string CardTitle
+        {
+            get => (string)GetValue(CardTitleProperty);
+            set => SetValue(CardTitleProperty, value);
+        }
+
+        public string CardDescription
+        {
+            get => (string)GetValue(CardDescriptionProperty);
+            set => SetValue(CardDescriptionProperty, value);
+        }
+
+        public Color TextColor
+        {
+            get => (Color)GetValue(TextColorProperty);
+            set => SetValue(TextColorProperty, value);
+        }
+
+        public CardView()
+        {
+            InitializeComponent();
+            BindingContext = this; // Ensure BindingContext is set
+        }
+    }
+}
