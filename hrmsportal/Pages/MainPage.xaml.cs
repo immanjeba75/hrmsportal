4e214f235e8773a20103f2cce681d1e10a4b410f
vigneshravi326 <vignesh.r@exchange-data.in>
2024-08-20 17:37:56 +0530

    Added Model, viewModel, Service

--- hrmsportal/Pages/MainPage.xaml.cs
+++ hrmsportal/Pages/MainPage.xaml.cs
@@ -1,43 +1,11 @@
 ﻿namespace hrmsportal.Pages
 {
-<<<<<<< HEAD
 	
 	public MainPage()
 	{
 		InitializeComponent();
-        BindingContext = new MainPageViewModel();
     }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    public class MainPageViewModel : INotifyPropertyChanged
-=======
-    public partial class MainPage : ContentPage
->>>>>>> bf76067 (Added Model, viewModel, Service)
-    {
-        public MainPage()
-        {
-            InitializeComponent();
-        }
-    }
-<<<<<<< HEAD
 
-    void Entry_Focused(System.Object sender, Microsoft.Maui.Controls.FocusEventArgs e)
-    {
-=======
-    private void btnLogin_Clicked(object sender, EventArgs e)
-    {
-		DisplayAlert("Login", "Login Clicked", "Ok");
->>>>>>> 4896b53 (push)
-=======
-    private void btnLogin_Clicked(object sender, EventArgs e)
-    {
-		DisplayAlert("Login", "Login Clicked", "Ok");
->>>>>>> 4896b53 (push)
-    }
 }
 
-
-=======
-}
->>>>>>> bf76067 (Added Model, viewModel, Service)
