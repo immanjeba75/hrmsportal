fd27e62bae01640f3e06f7bce3e0bf7931790bc6
immanuveljeba <immanuveljebakumar.v@exchange-data.in>
2024-08-20 11:24:10 +0530

    login screen and signup screen changes

--- hrmsportal/MauiProgram.cs
+++ hrmsportal/MauiProgram.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
-
+using CommunityToolkit.Maui;
 namespace hrmsportal;
 
 public static class MauiProgram
@@ -9,11 +9,13 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+			 .UseMauiCommunityToolkit()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
+		
 
 #if DEBUG
 		builder.Logging.AddDebug();
--- hrmsportal/Pages/MainPage.xaml.cs
+++ hrmsportal/Pages/MainPage.xaml.cs
@@ -1,14 +1,77 @@
 ﻿namespace hrmsportal.Pages;
-
+using System.ComponentModel;
+using System.Windows.Input;
+using Microsoft.Maui.Controls;
 public partial class MainPage : ContentPage
 {
 	
 	public MainPage()
 	{
 		InitializeComponent();
-	}
+        BindingContext = new MainPageViewModel();
+    }
 
-	
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        private bool _isLoginVisible;
+        private bool _isSignupVisible;
+
+        public bool IsLoginVisible
+        {
+            get => _isLoginVisible;
+            set
+            {
+                _isLoginVisible = value;
+                OnPropertyChanged(nameof(IsLoginVisible));
+            }
+        }
+
+        public bool IsSignupVisible
+        {
+            get => _isSignupVisible;
+            set
+            {
+                _isSignupVisible = value;
+                OnPropertyChanged(nameof(IsSignupVisible));
+            }
+        }
+
+        public ICommand ShowLoginCommand { get; }
+        public ICommand ShowSignupCommand { get; }
+
+        public MainPageViewModel()
+        {
+            // Set initial visibility
+            IsLoginVisible = true;
+            IsSignupVisible = false;
+
+            ShowLoginCommand = new Command(ShowLogin);
+            ShowSignupCommand = new Command(ShowSignup);
+        }
+
+        private void ShowLogin()
+        {
+            IsLoginVisible = true;
+            IsSignupVisible = false;
+        }
+
+        private void ShowSignup()
+        {
+            IsLoginVisible = false;
+            IsSignupVisible = true;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    void Entry_Focused(System.Object sender, Microsoft.Maui.Controls.FocusEventArgs e)
+    {
+    }
 }
 
 
